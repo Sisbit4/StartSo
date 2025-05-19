c31421885b98d0e61814349ea7dc6c34a310e1ca
Sonya <Sonya@SONYANOOTBOOK>
2025-05-19 19:48:37 +0500

    Задание 14 выполнено

--- LibraryTravel/Tourist.cs
+++ LibraryTravel/Tourist.cs
@@ -11,15 +11,18 @@ namespace LibraryTravel
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public readonly string TourCode; public DateTime StartDate { get; set; }
+        public readonly string TourCode; 
+        public DateTime StartDate { get; set; }
         public int TourDuration { get; set; }
         public double Cost { get; set; }
         public PayType PayType { get; set; }
         public DateTime EndDate => StartDate.AddDays(TourDuration);
         public Tourist(string name, string surname, int duration, string code, PayType pay)
         {
-            Name = name; Surname = surname;
-            TourDuration = duration; TourCode = code;
+            Name = name; 
+            Surname = surname;
+            TourDuration = duration; 
+            TourCode = code;
             PayType = pay;
         }
         public virtual string GetInfo()
@@ -30,6 +33,7 @@ namespace LibraryTravel
                 pay = "банковская карта";
             else
                 pay = "перечисление по счету";
+
             return $"Имя: {Name} {Surname}, Код тура: {TourCode}, Дата начала: {StartDate:d}, Продолжительность: {TourDuration} дней, Дата окончания: {EndDate:d}, Стоимость: {Cost}, Тип оплаты: {pay}";
         }
     }
