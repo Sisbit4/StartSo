5edf3c4d37946ca03c06dc5d18297a88233a3afe
Sonya <Sonya@SONYANOOTBOOK>
2025-05-23 00:59:25 +0500

    Задание 16 выполнено

---
+++ Circle/Circle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleStruct
+{
+    public struct Circle
+    {
+        private const double Tolerance = 1e-13;
+
+        public double X { get; }
+        public double Y { get; }
+        public double R { get; }
+
+        public double Length => 2 * Math.PI * R;
+        public double Area => Math.PI * R * R;
+
+        public Circle(double x, double y, double r)
+        {
+            if (r <= 0)
+                throw new ArgumentException("Радиус должен быть положительным числом.");
+
+            X = x;
+            Y = y;
+            R = r;
+        }
+
+        public override string ToString() =>
+            $"Окружность с радиусом {R} с центром в точке ({X}; {Y})";
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Circle other)
+            {
+                return Math.Abs(X - other.X) < Tolerance &&
+                       Math.Abs(Y - other.Y) < Tolerance &&
+                       Math.Abs(R - other.R) < Tolerance;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + X.GetHashCode();
+                hash = hash * 23 + Y.GetHashCode();
+                hash = hash * 23 + R.GetHashCode();
+                return hash;
+            }
+        }
+
+        public static Circle operator *(double k, Circle c) =>
+            new Circle(c.X, c.Y, c.R * k);
+
+        public static Circle operator *(Circle c, double k) => k * c;
+    }
+}
---
+++ CircleStruct.UnitTests/CircleTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CircleStruct;
+
+namespace CircleStruct.UnitTests
+{
+    [TestFixture]
+    public class CircleTests
+    {
+        [Test]
+        public void ConstructorRadiusTest()
+        {
+            var circle = new Circle(1.5, -2.3, 4.0);
+            Assert.That(circle.X, Is.EqualTo(1.5));
+            Assert.That(circle.Y, Is.EqualTo(-2.3));
+            Assert.That(circle.R, Is.EqualTo(4.0));
+        }
+
+        [Test]
+        public void ConstructorTest()
+        {
+            Assert.Throws<ArgumentException>(() => new Circle(0, 0, -1.0));
+        }
+
+        [Test]
+        public void LengthTest()
+        {
+            var circle = new Circle(0, 0, 1.0);
+            Assert.That(circle.Length, Is.EqualTo(2 * Math.PI).Within(1e-13));
+        }
+
+        [Test]
+        public void AreaTest()
+        {
+            var circle = new Circle(0, 0, 2.0);
+            Assert.That(circle.Area, Is.EqualTo(4 * Math.PI).Within(1e-13));
+        }
+
+        [Test]
+        public void EqualsTest()
+        {
+            var circle1 = new Circle(1.0, 2.0, 3.0);
+            var circle2 = new Circle(1.0, 2.0, 3.0);
+            Assert.That(circle1.Equals(circle2), Is.True);
+        }
+
+        [Test]
+        public void EqualsTest2()
+        {
+            var circle1 = new Circle(1.0, 2.0, 3.0);
+            var circle2 = new Circle(1.0, 2.0, 4.0);
+            Assert.That(circle1.Equals(circle2), Is.False);
+        }
+
+        [Test]
+        public void MultiplicationTest()
+        {
+            var circle = new Circle(1.0, 1.0, 2.0);
+            var scaledCircle = 3.0 * circle;
+            Assert.That(scaledCircle.R, Is.EqualTo(6.0));
+        }
+    }
+}
